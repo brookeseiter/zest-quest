7107fb4de34d63adcbc9ff177ea18e2000874849
Brooke Seiter <bseiter@ucsd.edu>
2024-03-11 19:14:48 -0700

    imported data models to crud.py and initialized flask application and home routes in routes.py

---
+++ server/crud.py
@@ -0,0 +1,4 @@
+"""CRUD operations."""
+
+from model import db, Player, Restaurant, Game_Settings, Round_Results, connect_to_db
+# from sqlalchemy.sql import func, insert, update
---
+++ server/routes.py
@@ -0,0 +1,23 @@
+"""Route for Zest Quest app."""
+
+from flask import (Flask, render_template, request, jsonify)
+from model import connect_to_db, db
+# from flask_cors import CORS
+# from dotenv import load_dotenv
+import os 
+import crud
+
+# load_dotenv()
+
+# Create the Flask application
+app = Flask(__name__)
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+# CORS(app, supports_credentials=True)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    app.run(host="0.0.0.0", debug=True)
