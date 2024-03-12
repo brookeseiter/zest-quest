d630d732cf2845cfa586f2a48aed5568258e67a1
Brooke Seiter <bseiter@ucsd.edu>
2024-03-11 20:57:32 -0700

    client directory tracked

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
+// import NotFound from './Components/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Zest Quest App</p>
+    </div>
+  );
+}
+
+export default App;
