be844c9b2534bdcc2339fd4ccfd5db56eff360c3
Brooke Seiter <bseiter@ucsd.edu>
2024-03-13 10:42:41 -0700

    changed names of page files according to react component naming conventions

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Game from './pages/Game';
 // import NotFound from './Components/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Homepage/>}></Route>
+      <Route path='/game' element={<Game/>}></Route>
     </Routes>
   );
 }
