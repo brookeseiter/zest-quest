e8d2e62f337a4c4099ce0e24d6fbacb41c6e95bb
Brooke Seiter <bseiter@ucsd.edu>
2024-03-22 14:21:24 -0700

    created structure of categories form with checkbox input

--- client/src/pages/CategorySelect.js
+++ client/src/pages/CategorySelect.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 function CategorySelect() {
+    const [checked, setChecked] = useState(false);
+
+    const handleChecked = () => {
+        setChecked(!checked);
+    };
+
     return (
         <>
-        <div>
-            <p>Category Selection Screen</p>
-        </div>
+            <div>
+            <h1>Choose restaurant categories:</h1>
+                <form className="categories-form">
+                    <label htmlFor="">
+                        Restaurant Category Choices:
+                        <input
+                            type="checkbox"
+                            onChange={handleChecked}
+                            required
+                        />
+                            Type of Restaurant
+                    </label>
+                    <p>Data type of "checked"? {typeof(checked)}</p>
+                    <p>Is "Type of Restaurant" checked? {checked.toString()}</p>
+                    <button>Submit Categories</button>
+                </form>
+            </div>
         </>
     );
 }
