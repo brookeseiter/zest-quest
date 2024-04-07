8adba1fa51c366f62d0bf8367992bbf752eb4e6f
Brooke Seiter <bseiter@ucsd.edu>
2024-04-07 16:57:28 -0700

    implemented a limit of 3 restaurant categories on category select page. able to append category names to chosen, not able to remove them if name is unchecked

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -1,16 +1,35 @@
-function Checkbox({label, name, value, onChange}) {
-    return ( 
-        <label htmlFor="">
-            <input
-                type="checkbox"
-                name={name}
-                checked={value}
-                onChange={onChange}
-                required
-            />
-                {label}
-        </label>
-    );
-}
+// function Checkbox({label, name, value, onChange, index}) {
+//     return ( 
+//         <label htmlFor="">
+//             <input
+//                 type="checkbox"
+//                 name={name}
+//                 checked={value}
+//                 onChange={onChange}
+//                 required
+//             />
+//                 {label}
+//         </label>
+//     );
+// }
 
-export default Checkbox;
+// export default Checkbox;
+
+
+// OG
+// function Checkbox({label, name, value, onChange}) {
+//     return ( 
+//         <label htmlFor="">
+//             <input
+//                 type="checkbox"
+//                 name={name}
+//                 checked={value}
+//                 onChange={onChange}
+//                 required
+//             />
+//                 {label}
+//         </label>
+//     );
+// }
+
+// export default Checkbox;
--- client/src/pages/CategorySelect.js
+++ client/src/pages/CategorySelect.js
@@ -1,37 +1,152 @@
 import { useState } from "react";
-import Checkbox from "../components/Checkbox";
+// import Checkbox from "../components/Checkbox";
+
+// const categories = [
+//     {
+//       name: "American",
+//       price: 1.2
+//     },
+//     {
+//       name: "BBQ",
+//       price: 2.0
+//     },
+//     {
+//       name: "Fast Food",
+//       price: 2.5
+//     },
+//     {
+//       name: "Chinese",
+//       price: 3.0
+//     }
+// ];
+const categories = ["American","BBQ","Fast Food","Chinese"];
+
+// const getFormattedPrice = (price) => `$${price.toFixed(2)}`;
 
 function CategorySelect() {
-    const [american, setAmerican] = useState(false);
-    const [bbq, setBbq] = useState(false);
-    const [chinese, setChinese] = useState(false);
-    const [fastFood, setFastFood] = useState(false);
+    // const [american, setAmerican] = useState(false);
+    // const [bbq, setBbq] = useState(false);
+    // const [chinese, setChinese] = useState(false);
+    // const [fastFood, setFastFood] = useState(false);
 
-    const handleAmerican = (e) => {
-        setAmerican(e.target.checked);
-        console.log(e.target.checked, e.target.name);
+    // const handleAmerican = (e) => {
+    //     setAmerican(e.target.checked);
+    //     console.log(e.target.checked, e.target.name);
+    // };
+
+    // const handleBbq = (e) => {
+    //     setBbq(e.target.checked);
+    //     console.log(e.target.checked, e.target.name);
+    // };
+
+    // const handleChinese = (e) => {
+    //     setChinese(e.target.checked);
+    //     console.log(e.target.checked, e.target.name);
+    // };
+
+    // const handleFastFood = (e) => {
+    //     setFastFood(e.target.checked);
+    //     console.log(e.target.checked, e.target.name);
+    // };
+
+    // const [total, setTotal] = useState(0);
+
+    const [category1, setCategory1] = useState("");
+    const [category2, setCategory2] = useState("");
+    const [category3, setCategory3] = useState("");
+    const [chosen, setChosen] = useState([]);
+
+    const [checkedState, setCheckedState] = useState(
+        new Array(categories.length).fill(false)
+    );
+    console.log('checkedState', checkedState);
+
+    const handleOnChange = (position, e) => {
+        if (checkedState.filter((i) => i).length >= 3 && e.target.checked) return;
+
+        const updatedCheckedState = checkedState.map((item, index) =>
+            index === position ? !item : item
+        );
+    
+        setCheckedState(updatedCheckedState);
+    
+        // const totalPrice = updatedCheckedState.reduce((sum, currentState, index) => {
+        //     if (currentState === true) {
+        //         return sum + categories[index].price;
+        //     }
+        //     return sum;
+        // }, 0);
+    
+        // setTotal(totalPrice);
     };
 
-    const handleBbq = (e) => {
-        setBbq(e.target.checked);
-        console.log(e.target.checked, e.target.name);
-    };
+    // const submitSettings = (e) => {
+    //     e.preventDefault();
+    //     const categoriesJSON = {
+    //         'category_1': numPlayers,
+    //         'category_2': location,
+    //         'category_3': maxDist
+    //     };
 
-    const handleChinese = (e) => {
-        setChinese(e.target.checked);
-        console.log(e.target.checked, e.target.name);
-    };
+    //     const requestOptions = {
+    //         method: 'POST',
+    //         headers: {'content-type': 'application/json'},
+    //         body: JSON.stringify(categoriesJSON)
+    //     };
 
-    const handleFastFood = (e) => {
-        setFastFood(e.target.checked);
-        console.log(e.target.checked, e.target.name);
-    };
+    //     console.log('requestOptions', requestOptions);
+        
+    //     fetch(`/categories`, requestOptions)
+    //     .then((response) => response.json())
+    //     .then((categoriesData) => {
+    //         console.log('categoriesData', categoriesData);
+    //         navigate('/load');
+    //     })
+    //     .catch((error) => {
+    //         console.log('error: ', error);
+    //     }, []); 
+    // };
+    console.log('chosen',chosen);
+
 
     return (
         <>
             <div>
             <h1>Choose restaurant categories:</h1>
                 <form className="categories-form">
+                    <ul className="toppings-list">
+                        {/* {categories.map(({ name, price }, index) => { */}
+                        {categories.map((name, index) => {
+                            return (
+                                <li key={index}>
+                                    <div className="toppings-list-item">
+                                        <div className="left-section">
+                                            <input
+                                                type="checkbox"
+                                                id={`custom-checkbox-${index}`}
+                                                name={name}
+                                                value={name}
+                                                checked={checkedState[index]}
+                                                // onChange={(e) => handleOnChange(index, e)}
+                                                onChange={(e) => {
+                                                    handleOnChange(index, e);
+                                                    chosen.push(name);
+                                                }}
+                                            />
+                                            <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                                        </div>
+                                        {/* <div className="right-section">{getFormattedPrice(price)}</div> */}
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    {/* <li>
+                        <div className="toppings-list-item">
+                            <div className="left-section">Total:</div>
+                            <div className="right-section">{getFormattedPrice(total)}</div>
+                        </div>
+                    </li> */}
+                    </ul>
                     {/* <label htmlFor="american">
                         <input
                             type="checkbox"
@@ -42,8 +157,8 @@ function CategorySelect() {
                             required
                         />
                             American
-                    </label> */}
-                    {/* <label htmlFor="">
+                    </label>
+                    <label htmlFor="">
                         <input
                             type="checkbox"
                             name="bbq"
@@ -52,7 +167,7 @@ function CategorySelect() {
                             required
                         />
                             BBQ
-                    </label> */}
+                    </label>
                     <Checkbox
                         label="American"
                         name="american"
@@ -77,7 +192,7 @@ function CategorySelect() {
                         value={fastFood}
                         onChange={handleFastFood}
                     />
-                    {/* <Checkbox
+                    <Checkbox
                         label="Indian"
                         value={checked}
                         onChange={handleChecked}
@@ -129,6 +244,8 @@ function CategorySelect() {
                     /> */}
                     <button>Submit Categories</button>
                 </form>
+                <p>This is checkedState: {checkedState}</p>
+                <p>This is type of checkedState: typeof(checkedState)</p>
             </div>
         </>
     );
