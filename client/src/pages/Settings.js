5d77f9322536f3b6763eb26c011e8a9f78c500fb
Brooke Seiter <bseiter@ucsd.edu>
2024-07-15 14:24:53 -0700

    settings input style matches that of the select component except color of focused outline/border, removed custom-label and label-input text

--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -94,31 +94,23 @@ function Settings() {
                                         <Option value="4">4</Option>
                                         <Option value="5">5</Option>
                                     </Select>
-                                    {/* <div className="relative mt-4"> */}
-                                        <Input 
-                                            className="location-input flex peer w-full h-full text-blue-gray-700 font-sans font-normal text-left outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all border text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 border-t-transparent shadow-inner focus:outline-none bg-white" 
-                                            variant="static" 
-                                            value={location}
-                                            label="Address of your current location:" 
-                                            onChange={(e) => setLocation(e.target.value)}
-                                            id="location" 
-                                            size="lg" 
-                                            required 
-                                            containerProps={{
-                                                className: 'label-hidden-required-span'    
-                                            }}
-                                            labelProps={{ className: "custom-label" }} 
-                                        />
-                                        <div class="relative w-full min-w-[200px] h-10">
+                                    <div className="relative mt-4">
+                                        <div className="relative w-full min-w-[200px] h-10">
                                             <input
-                                            class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500"
-                                            placeholder=" " />
-                                            <label
-                                                class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-blue-500 before:border-blue-gray-200 peer-focus:before:!border-blue-500 after:border-blue-gray-200 peer-focus:after:!border-blue-500">Input
-                                                Blue
-                                            </label>
+                                                className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-transparent border focus:border-2 border-t-transparent focus:border-blue-500 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-t-transparent"
+                                                placeholder=" "
+                                                value={location}
+                                                onChange={(e) => setLocation(e.target.value)}
+                                                id="location" 
+                                                required
+                                            />
+                                                <label
+                                                    className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate text-[11px] leading-tight text-blue-gray-400 peer-focus:text-blue-500 before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 before:border-t peer-focus:before:border-t-2 before:border-l before:border-blue-gray-200 before:rounded-tl-md before:pointer-events-none before:transition-all peer-focus:before:border-blue-500 after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 after:border-t peer-focus:after:border-t-2 after:border-r after:border-blue-gray-200 after:rounded-tr-md after:pointer-events-none after:transition-all peer-focus:after:border-blue-500 -top-1.5 peer-placeholder-shown:-top-1.5 peer-focus:-top-1.5"
+                                                >
+                                                    Address of your current location:
+                                                </label>
                                         </div>
-                                    {/* </div> */}
+                                    </div>
                                 </div>
                                 <div className="flex flex-col h-full w-1/2">
                                     <Typography>How far are you willing to go?</Typography>
