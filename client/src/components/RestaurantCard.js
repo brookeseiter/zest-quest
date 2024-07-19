2a83d95e785a0ec9f5db5b5285b29353f0d43a9c
Brooke Seiter <bseiter@ucsd.edu>
2024-07-18 19:33:26 -0700

    styled RestaurantCard, changed colors to orange, added current calculated distance from player location, separated categories with dots

--- client/src/components/RestaurantCard.js
+++ client/src/components/RestaurantCard.js
@@ -11,47 +11,11 @@ import {
     Button 
 } from "@material-tailwind/react";
 
-
-function RestaurantCard({ classStyles, restaurant })  {
+function RestaurantCard({ restaurant })  {
+    const distanceMiles = restaurant.distance * 0.00062137;
     console.log(restaurant);
     return (
         <>
-            {/* <Card className={`h-2/5 flex flex-col justify-start items-center w-96 bg-white rounded-2xl gap-5 cursor-pointer transition ease-in-out duration-300 hover:scale-105 hover:-translate-y-1 ${classStyles}`}>
-                <CardHeader className="h-3/5 w-3/5 flex justify-center items-center flex-grow-0 flex-shrink-0">
-                    {restaurant.image_url && (
-                        <img 
-                            // className="h-full w-full object-cover md:h-48 lg:h-56"
-                            className="size-full s-42"
-                            src={restaurant.image_url}
-                            alt={`${restaurant.name}`}
-                        />
-                    )}
-                </CardHeader>
-                <CardBody className="h-2/5 w-full flex-grow overflow-y-auto p-4">
-                    <h1>
-                        {restaurant.name}
-                    </h1>
-                    <h1>
-                        {restaurant.rating}
-                    </h1>
-                    <h1>
-                        {restaurant.price}
-                    </h1>
-                    <h1>
-                        {restaurant.address}
-                    </h1>
-                    <div> 
-                        {restaurant.categories.map((category, i) => (
-                            <p className="flex flex-col" key={i}>
-                                {i !== restaurant.categories.length - 1 ? category.title + ',' : category.title}
-                            </p>
-                        ))}
-                    </div>
-                    <p>
-                        {restaurant.location.address1}
-                    </p>
-                </CardBody>
-            </Card> */}
             <div className="flex w-full h-full justify-center items-center antialiased text-gray-900">
                 <div className="w-full transition ease-in-out duration-300 hover:scale-105 hover:-translate-y-1 ">
                     {restaurant.image_url && (
@@ -70,20 +34,22 @@ function RestaurantCard({ classStyles, restaurant })  {
                                     {restaurant.price}
                                 </span>
                                 <div className="ml-2 text-gray-600 uppercase text-xs font-semibold tracking-wider">
-                                    2 baths  &bull; 3 rooms
+                                    {distanceMiles.toFixed(2)} mi away
                                 </div>  
                             </div>
                             <h4 className="mt-1 text-xl font-semibold uppercase leading-tight truncate">{restaurant.name}</h4>
                             <div className="flex justify-center mt-1">
-                                {restaurant.categories.map((category, i) => (
-                                    <p className="flex flex-col" key={i}>
-                                        {i !== restaurant.categories.length - 1 ? category.title + ',' : category.title}
-                                    </p>
-                                ))}
+                                <p className="flex flex-wrap text-base font-light">
+                                    {restaurant.categories.map((category, i) => (
+                                        <span key={i} className="inline">
+                                            {category.title}{i !== restaurant.categories.length - 1 && ' •\u00A0'}
+                                        </span>
+                                    ))}
+                                </p>
                             </div>
                             <div className="mt-4">
                                 <span className="text-[#eb986f] text-md font-bold">{restaurant.rating}/5 rating </span>
-                                {/* <span class="text-sm text-gray-600">({restaurant.review_count} reviews)</span> */}
+                                <span class="text-sm text-gray-600">({restaurant.review_count} reviews)</span>
                             </div>  
                         </div>
                     </div>
